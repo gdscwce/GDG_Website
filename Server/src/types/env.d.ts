db6c691b13c1a3fe6def1e013d4010ddc45ab643
dhanraj-12 <dj004786@gmail.com>
2025-12-13 00:47:27 +0530

    by_dj_middelware_and_s3client

--- Server/src/types/env.d.ts
+++ Server/src/types/env.d.ts
@@ -2,5 +2,10 @@ declare namespace NodeJS {
     interface ProcessEnv {
         PORT : string
         MONGO_URI:string
+        AWS_REGION:string
+        AWS_ACCESS_KEY_ID:string
+        AWS_SECRET_ACCESS_KEY:string
+        
+
     }
   }
