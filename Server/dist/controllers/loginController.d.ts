8953e64b45201917ae405e1047d7298d67f4b638
shripad_69 <shripadalgude@gmail.com>
2025-12-12 23:13:41 +0530

    by_shri_removed_dist

--- Server/dist/controllers/loginController.d.ts
+++
@@ -1,3 +0,0 @@
-import type { Request, Response } from "express";
-export declare function adminLogin(req: Request, res: Response): Promise<Response<any, Record<string, any>>>;
-//# sourceMappingURL=loginController.d.ts.map
--- Server/dist/models/yearGroupModel.js
+++
@@ -1,8 +0,0 @@
-import { Schema, model, Types, Document } from "mongoose";
-const YearGroupSchema = new Schema({
-    year: { type: String, required: true },
-    events: [{ type: Schema.Types.ObjectId, ref: "Event" }],
-    members: [{ type: Schema.Types.ObjectId, ref: "Member" }],
-});
-export const yearGroupModel = model("YearGroup", YearGroupSchema);
-//# sourceMappingURL=yearGroupModel.js.map
